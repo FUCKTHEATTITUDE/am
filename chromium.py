c2889f32a6cda5a32bae4154953f02bc975a79c8
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-30 17:29:17 +0530

    Update chromium.py

--- chromium.py
+++ chromium.py
@@ -138,7 +138,10 @@ def zoom(update, context):
 		time.sleep(5)
 		browser.find_element_by_xpath('//*[@id="inputpasscode"]').send_keys(passStr)
 		browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
-		
+		time.sleep(5)
+		browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[1]/div[2]/div[2]/div[1]/button/svg').click()
+		time.sleep(5)
+		browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[1]/button').click()
 
 		time.sleep(15)
 		
