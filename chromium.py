0f3058798f4b0009eef90778ca84df2f6dc3d734
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-30 15:39:16 +0530

    Update chromium.py

--- chromium.py
+++ chromium.py
@@ -53,6 +53,7 @@ options.add_experimental_option("prefs", { \
      "profile.default_content_setting_values.notifications": 1
   })
 
+
 browser = webdriver.Chrome(options=options)
 logged_in=False
 teams_in=False
@@ -83,55 +84,86 @@ def status(update, context):
 	
 def zoom(update, context):
 	logging.info("DOING")
-	
 	try:
-	    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+		context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 		
-	    meetingcode = '3069823402'
-	    passcode = 'WARP1d'
+		usernameStr = Config.USERNAME
+		passwordStr = Config.PASSWORD
 
-	    url_meet = update.message.text.split()[1]
-	    passStr = update.message.text.split()[2]
+		url_meet = update.message.text.split()[1]
+		passStr = update.message.text.split()[2]
 
-		
-		
-            browser.get('https://zoom.us/wc/join/'+ url_meet)
-			
-                
-	    time.sleep(5)
-            browser.save_screenshot("ss.png")
-	    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
-	    mid  = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), caption="Test", timeout = 120).message_id
-	    os.remove('ss.png')
-	    inp = browser.find_element(By.ID, 'inputname')
-            time.sleep(3)
-            inp.send_keys(f"{user}")
-            btn2 = browser.find_element(By.ID, 'joinBtn')
-            btn2.click()
-            time.sleep(2)
-            inp2 = browser.find_element(By.ID, 'inputpasscode')
-            time.sleep(1)
-            inp2.send_keys(passcode)
-            btn3 = browser.find_element(By.ID, 'joinBtn')
-            time.sleep(1)
-            btn3.click()
+		if os.path.exists("zoom.pkl"):
+			cookies = pickle.load(open("zoom.pkl", "rb"))
+			browser.get('https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Afad07e7074c3d678%2C10%3A1601127482%2C16%3A9619c3b16b4c5287%2Ca234368b2cab7ca310430ff80f5dd20b5a6a99a5b85681ce91ca34820cea05c6%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22d18871cbc2a3450c8c4114690c129bde%22%7D&response_type=code&flowName=GeneralOAuthFlow')
+			for cookie in cookies:
+				browser.add_cookie(cookie)
+		else:
+			browser.get('https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Afad07e7074c3d678%2C10%3A1601127482%2C16%3A9619c3b16b4c5287%2Ca234368b2cab7ca310430ff80f5dd20b5a6a99a5b85681ce91ca34820cea05c6%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22d18871cbc2a3450c8c4114690c129bde%22%7D&response_type=code&flowName=GeneralOAuthFlow')
+			username = browser.find_element_by_id('identifierId')
+			username.send_keys(usernameStr)
+			nextButton = browser.find_element_by_id('identifierNext')
+			nextButton.click()
+			time.sleep(7)
 
-	
+			browser.save_screenshot("ss.png")
+			context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+			mid = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), timeout = 120).message_id
+			os.remove('ss.png')
 
-	     time.sleep(10)
+			password = browser.find_element_by_xpath("//input[@class='whsOnd zHQkBf']")
+			password.send_keys(passwordStr)
+			signInButton = browser.find_element_by_id('passwordNext')
+			signInButton.click()
+			time.sleep(7)
 
-	     context.bot.delete_message(chat_id=update.message.chat_id ,message_id = mid)
 
-	     browser.save_screenshot("ss.png")
-	     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
-	     mid = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), timeout = 120).message_id
-	     os.remove('ss.png')
+			browser.get('https://zoom.us/google_oauth_signin')
+			time.sleep(7)
 
-	     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-	     context.bot.send_message(chat_id=update.message.chat_id, text="Attending you lecture. You can chill ")
-	     context.bot.send_message(chat_id=update.message.chat_id,text="To exit click /exitmeet ")
-	     pause
-	     logging.info("STAAAAPH!!")
+			if(browser.find_elements_by_xpath('//*[@id="authzenNext"]/div/button/div[2]')):
+				context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+				context.bot.send_message(chat_id=update.message.chat_id, text="Need Verification. Please Verify")
+				browser.find_element_by_xpath('//*[@id="authzenNext"]/div/button/div[2]').click()
+				time.sleep(5)
+
+				browser.save_screenshot("ss.png")
+				context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+				mid = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), timeout = 120).message_id
+				os.remove('ss.png')
+				time.sleep(20)
+
+			pickle.dump( browser.get_cookies() , open("zoom.pkl","wb"))
+
+			context.bot.send_message(chat_id=update.message.chat_id, text="Logged In!")
+
+		browser.get('https://zoom.us/wc/join/'+ url_meet)
+
+		time.sleep(5)
+		browser.save_screenshot("ss.png")
+		context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+		mid  = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), caption="Test", timeout = 120).message_id
+		os.remove('ss.png')
+
+		browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
+		time.sleep(5)
+		browser.find_element_by_xpath('//*[@id="inputpasscode"]').send_keys(passStr)
+		browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
+
+		time.sleep(10)
+
+		context.bot.delete_message(chat_id=update.message.chat_id ,message_id = mid)
+
+		browser.save_screenshot("ss.png")
+		context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_PHOTO)
+		mid = context.bot.send_photo(chat_id=update.message.chat_id, photo=open('ss.png', 'rb'), timeout = 120).message_id
+		os.remove('ss.png')
+
+		context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+		context.bot.send_message(chat_id=update.message.chat_id, text="Attending you lecture. You can chill ")
+		context.bot.send_message(chat_id=update.message.chat_id,text="To exit click /exitmeet ")
+		pause
+		logging.info("STAAAAPH!!")
 
 	
 	except:
@@ -317,7 +349,7 @@ def gchat(update,context):
 
 
 def start(update,context):
-	context.bot.send_message(chat_id=update.message.chat_id,text="Use following Commands to interact with bot :\nTo join zoom meeting - /zoom zoommeetingid password\nTo know Status of Bot - /status\nTo exit meet - /exitmeet\nTo restart Bot🤖 - /restart")
+	context.bot.send_message(chat_id=update.message.chat_id,text="Use following Commands to interact with bot :\nTo join google meet - /meet Gmeetlink\n To Send messages in Gmeet - /gchat yourmessage \nTo join zoom meeting - /zoom zoommeetingid password\nTo know Status of Bot - /status\nTo exit Gmeet - /exitmeet\nTo restart Bot🤖 - /restart")
 	
 def loginteams(update,context):
 	logging.info("logging in teams account")
