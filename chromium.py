2d4f14f902f7f3da3d49368d8deec84667bff040
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-31 10:32:18 +0530

    Update chromium.py

--- chromium.py
+++ chromium.py
@@ -503,25 +503,6 @@ def exitteams(update,context):
 
 def main():
 	import os
-	wait_time = sec * 60
-        workers = []
-        for i in range(number):
-          fakes = fake[i]
-          try:
-             proxy = proxylist[i]
-          except IndexError:
-             proxy = None
-          try:
-             user = fakes
-          except IndexError:
-             break
-        wk = threading.Thread(target=start, args=(
-            f'[Thread{i}]', proxy, user, wait_time))
-        workers.append(wk)
-        for wk in workers:
-            wk.start()
-        for wk in workers:
-            wk.join()
 	# PORT = int(os.environ.get('PORT', 8000))
 	dp.add_handler(CommandHandler("start",start))
 	dp.add_handler(CommandHandler("zoom", zoom))
